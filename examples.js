3104990a8fc6297be2f26641c7f0a19e318d475e
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-16 06:58:42 +0400

    installed express

--- examples.js
+++ examples.js
@@ -22,32 +22,45 @@
 // });
 // request.end();
 //------------------------------------------------------------------
-//Example - Async/Await
-const axios = require('axios');
+// //Example - Async/Await
+// const axios = require('axios');
 
-const connectToURL = (url) => {
-	const req = axios.get(url); //send get req; returns promise
-	console.log(req);
-	req
-		.then((res) => {
-			let listOfEntries = res.data.entries;
-			listOfEntries.forEach((entry) => {
-				console.log(entry.Category);
-			});
-		})
-		.catch((err) => {
-			console.log(err.message);
-		});
-};
-console.log('Before connect URL');
-connectToURL('https://api.publicapis.org/entries');
-console.log('After connect URL');
-//Same example using async/await
-const connectToURLasync = async (url) => {
-	const outcome = axios.get(url);
-	let listOfEntries2 = (await outcome).data.entries;
-	listOfEntries2.forEach((entry) => {
-		console.log(entry.Category);
-	});
-};
-connectToURLasync('https://api.publicapis.org/entries');
+// const connectToURL = (url) => {
+// 	const req = axios.get(url); //send get req; returns promise
+// 	console.log(req);
+// 	req
+// 		.then((res) => {
+// 			let listOfEntries = res.data.entries;
+// 			listOfEntries.forEach((entry) => {
+// 				console.log(entry.Category);
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.log(err.message);
+// 		});
+// };
+// console.log('Before connect URL');
+// connectToURL('https://api.publicapis.org/entries');
+// console.log('After connect URL');
+// //Same example using async/await
+// const connectToURLasync = async (url) => {
+// 	const outcome = axios.get(url);
+// 	let listOfEntries2 = (await outcome).data.entries;
+// 	listOfEntries2.forEach((entry) => {
+// 		console.log(entry.Category);
+// 	});
+// };
+// connectToURLasync('https://api.publicapis.org/entries');
+//------------------------------------------------------------------
+// //Example -  simple express server
+const express = require('express');
+const app = express(); //create instance (obj) of express framework API
+//use {app} to set up your server, define routes, and listen for incoming requests.
+const port = 3000;
+app.get('/', (req, req) => {
+	//logic
+});
+//start HTTP server
+let server = app.listen(port, () => {
+	console.log(`Server is listening on URL http://localhost:${port}`);
+});
