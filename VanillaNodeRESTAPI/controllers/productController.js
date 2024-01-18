1d8cb1b5cc942dfac6375cf56f8c1f29655176ae
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-18 08:19:36 +0400

    contd writing logic for server, models, controller, utils - instead of hardcording/using static files  for create function want to add body to req when posting + added getPostData (updating) in utils - refactored createProducts using utils function

--- VanillaNodeRESTAPI/controllers/productController.js
+++ VanillaNodeRESTAPI/controllers/productController.js
@@ -1,6 +1,6 @@
 //handle Routing Logic and interact with model data
 const Product = require('../productModels');
-
+const { getPostData } = require('../utils.js');
 //note:async because model function returns a promise
 //GET all products /api/products
 async function getProducts(req, res) {
@@ -28,19 +28,62 @@ async function getProduct(req, res, id) {
 	}
 }
 //POST new product /api/products
+// async function createProduct(req, res) {
+// 	try {
+// 		//---------------------
+// 		// const product = {
+// 		// 	title: 'test static product',
+// 		// 	description: 'test inserting static product',
+// 		// 	price: 100,
+// 		// 	//generate id with uuid package in productModels.js
+// 		// };
+// 		//---------------------
+// 		//use body of req instead of static/hardcoded new product data
+// 		let body = '';
+// 		//.on is method to listen for events, param1 (data event in this case) which is emitted when a 'chunk' of the req body has been received.
+// 		//test by adding body with props in POST req
+// 		req.on('data', (chunk) => {
+// 			//chunk returns a buffer containing the received data
+// 			body += chunk.toString(); //convert buffer to string
+// 		});
+// 		req.on('end', async () => {
+// 			const { title, description, price } = JSON.parse(body); //destructure body of req body of product
+
+// 			const product = {
+// 				title,
+// 				description,
+// 				price,
+// 			};
+// 			const newProduct = await Product.create(product);
+// 			res.writeHead(201, { 'Content-type': 'application/json' });
+// 			return res.end(JSON.stringify(newProduct));
+// 		});
+// 		//note - a buffer class provides way to work with diff kinds of binary data. Used to read/write data to streams, manipulate binary data from files and handle data from network protocols. It is global obj so dont need to import with require
+
+// 		// placed inside req.on after adding logic to serve non-static files
+// 		// const newProduct = await Product.create(product);
+// 		// res.writeHead(201, { 'Content-type': 'application/json' });
+// 		// return res.end(JSON.stringify(newProduct));
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
+//---------------------------------------
+////POST new product /api/products - refactored using utils functions
 async function createProduct(req, res) {
 	try {
+		const body = await getPostData(req);
+		const { title, description, price } = JSON.parse(body);
 		const product = {
-			title: 'test static product',
-			description: 'test inserting static product',
-			price: 100,
-			//generate id with uuid package in productModels.js
+			title,
+			description,
+			price,
 		};
 		const newProduct = await Product.create(product);
 		res.writeHead(201, { 'Content-type': 'application/json' });
 		return res.end(JSON.stringify(newProduct));
-	} catch (error) {
-		console.log(error);
+	} catch (err) {
+		console.log(err);
 	}
 }
 module.exports = {
--- VanillaNodeRESTAPI/utils.js
+++ VanillaNodeRESTAPI/utils.js
@@ -7,6 +7,23 @@ function writeDataToFile(filename, content) {
 		}
 	});
 }
+
+function getPostData(req) {
+	return new Promise((resolve, reject) => {
+		try {
+			let body = '';
+			req.on('data', (chunk) => {
+				body += chunk.toString();
+			});
+			req.on('end', () => {
+				resolve(body);
+			});
+		} catch (err) {
+			reject(err);
+		}
+	});
+}
 module.exports = {
 	writeDataToFile,
+	getPostData,
 };
