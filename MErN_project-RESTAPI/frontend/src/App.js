2b0fba264366776a137e2a7c74426cab4544ed8b
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-19 18:33:58 +0400

    deleted notes.txt will add notes in App.js + created components folder in src --> created Header for navigation in components using router-dom {Link} +  installed react-icons package to use in Header links + needed to install @babel/plug-in... as dev dependency because of deprecated issue + commented out anything to do with store.js & Provider(index.js) - tested and page loads with nav bar and links to pages are working.

--- MErN_project-RESTAPI/frontend/src/App.js
+++ MErN_project-RESTAPI/frontend/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/dashboard';
-import Login from './pages/login';
-import Register from './pages/register';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<>
 			<Router>
 				<div class="container">
+					<Header />
 					<Routes>
 						<Route path="/" element={<Dashboard />} />
 						<Route path="/login" element={<Login />} />
@@ -20,3 +22,9 @@ function App() {
 }
 
 export default App;
+// #NOTES
+
+// Will not be using the counter in this project - left as example
+// --> Create userSlice.js + goalSlice.js that have reducers we add to store.js
+
+// For the App we need 3 pages in src folder - Login, Register, Dashboard (that you need to be logged in to access)
--- MErN_project-RESTAPI/frontend/src/app/store.js
+++ MErN_project-RESTAPI/frontend/src/app/store.js
@@ -1,5 +1,5 @@
 //imported in index.js - we add reducer from {<fileName>slice.js} and add it to store
-import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore } from '@reduxjs/toolkit';
 
 //-----------------------------------------------
 //we will not be using this reducer - we will create user + goal reducers for our app
