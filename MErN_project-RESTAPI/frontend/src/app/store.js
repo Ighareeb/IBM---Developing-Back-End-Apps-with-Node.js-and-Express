61c17da8982b496457014ad648cd552f5fc345f3
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-19 15:21:55 +0400

    got rid of default react/redux files + added css

--- MErN_project-RESTAPI/frontend/src/app/store.js
+++ MErN_project-RESTAPI/frontend/src/app/store.js
@@ -1,8 +1,12 @@
+//imported in index.js - we add reducer from {<fileName>slice.js} and add it to store
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+//-----------------------------------------------
+//we will not be using this reducer - we will create user + goal reducers for our app
+// import counterReducer from '../features/counter/counterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
