17326cc63853054bf050ae0d532cd7ee3c3ad9c1
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-19 20:49:14 +0400

    Started working on login + register pages.

--- MErN_project-RESTAPI/frontend/src/pages/Register.jsx
+++ MErN_project-RESTAPI/frontend/src/pages/Register.jsx
@@ -1,5 +1,87 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaUser } from 'react-icons/fa';
 
 export default function Register() {
-	return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '', //confirm password
+	});
+
+	const { name, email, password, password2 } = formData;
+
+	const handleChange = (e) => {
+		setFormData((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaUser />
+					Register
+				</h1>
+				<p>Please create an account</p>
+			</section>
+
+			<section className="form">
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<input
+							type="text"
+							value={name}
+							name="name"
+							id="name"
+							className="form-control"
+							placeholder="Enter your name"
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="email"
+							value={email}
+							name="email"
+							id="name"
+							className="form-control"
+							placeholder="Enter your email"
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							value={password}
+							name="password"
+							id="password"
+							className="form-control"
+							placeholder="Enter your password"
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							value={password2}
+							name="password2"
+							id="password2"
+							className="form-control"
+							placeholder="Confirm your password"
+						/>
+					</div>
+					<div className="form-group">
+						<button type="submit" className="btn btn-block">
+							Submit
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
