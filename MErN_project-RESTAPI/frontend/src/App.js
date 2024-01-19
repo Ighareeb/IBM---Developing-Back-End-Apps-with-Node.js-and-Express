8c386138a931d3488c80509f52f4f16bcdad598a
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-19 18:16:28 +0400

    started working on 3 pages needed for the App - added notes.txt to set targets + installed react router (npm i react-router-dom) --> started creating routes in App.js

--- MErN_project-RESTAPI/frontend/src/App.js
+++ MErN_project-RESTAPI/frontend/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+import Register from './pages/register';
+
 function App() {
 	return (
-		<div>
-			<h1>My App</h1>
-		</div>
+		<>
+			<Router>
+				<div class="container">
+					<Routes>
+						<Route path="/" element={<Dashboard />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+				</div>
+			</Router>
+		</>
 	);
 }
 
