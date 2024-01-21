85b631d38f9b63185b399ff6215f414e1648bfc1
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-21 17:51:06 +0400

    added dispatch actions addCases in authSlice for login + connect Login component TESTED OK but need to correct issue as logout still redirects to dashboard and need to change

--- MERN_project-RESTAPI/frontend/src/components/Header.jsx
+++ MERN_project-RESTAPI/frontend/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
 				{user ? (
 					<li>
 						<button className="btn" onClick={onLogout}>
-							<FaSignOutAlt />
+							<FaSignOutAlt /> Logout
 						</button>
 					</li>
 				) : (
--- MERN_project-RESTAPI/frontend/src/pages/Login.jsx
+++ MERN_project-RESTAPI/frontend/src/pages/Login.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, reset } from '../features/auth/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { FaSignInAlt } from 'react-icons/fa';
+import Spinner from '../components/Spinner';
+
 export default function Login() {
 	const [loginData, setLoginData] = useState({
 		email: '',
@@ -8,6 +14,23 @@ export default function Login() {
 	});
 
 	const { email, password } = loginData;
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth,
+	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+		if (isSuccess || user) {
+			navigate('/');
+		}
+		dispatch(reset());
+	}, [email, password, user, isError, isSuccess, message, navigate, dispatch]);
 
 	const handleChange = (e) => {
 		setLoginData((prev) => ({
@@ -18,7 +41,17 @@ export default function Login() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
+		const userData = {
+			email,
+			password,
+		};
+		dispatch(login(userData));
 	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
 	return (
 		<>
 			<section className="heading">
