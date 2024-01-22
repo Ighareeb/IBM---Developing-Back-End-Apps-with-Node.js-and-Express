fbfec694719e6dbf6b9a49c56bd43d61faf8497c
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-22 11:16:52 +0400

    Setting up deployment - server.js PROJECT COMPLETE

--- MERN_project-RESTAPI/backend/server.js
+++ MERN_project-RESTAPI/backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path'); //built in Node.js module
 const express = require('express'); //BE web framework
 const dotenv = require('dotenv').config();
 const errorHandler = require('./middleware/errorMiddleware');
@@ -18,6 +19,27 @@ app.use(express.urlencoded({ extended: false })); //Middleware that parses req t
 app.use('/api/goals', require('./routes/goalRoutes')); //goals endpoint
 app.use('/api/users', require('./routes/userRoutes')); //users endpoint
 
+//-----------------------------------
+//BE using express for dev server
+//FE dev server is just for development -  so we need to build FE (npm run build) for React --> creates build folder with static assets. This becomes entry point for FE that we need to point to.
+//serve FE
+
+if (process.env.NODE_ENV === 'production') {
+	//serve static folder i.e. build folder for React
+	app.use(express.static(path.join(__dirname, '../frontend/build'))); //build folder created when we run <npm run build>; ( __dirname is current directory )
+
+	//serve index.html file from build folder. we have already 'pointed' our api routes in above code so here we are pointing any other routes to our index.html file
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
+		);
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.send('Need to set to production mode in NODE_ENV to serve FE');
+	});
+}
+//-----------------------------------
 app.use(errorHandler); //this now overwrites default express errorHandler
 // app.use(authHandler); //so we can use JWT for authentication
 
